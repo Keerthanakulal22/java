538daa4ded56a50bb51d4cd4859230712e5de5d1
Keerthanakulal22 <keerthana.23cs057@sode-edu.in>
2024-11-12 14:14:31 +0530

    Add files via upload

---
+++ M7.java
@@ -0,0 +1,9 @@
+
+package pkg1;
+import pkg2.tools2;
+public class M7 {
+    public static void main(String[] args){
+        tools2 t1=new tools2();
+        System.out.println(t1.reverse("Keerthana"));
+    }
+}
