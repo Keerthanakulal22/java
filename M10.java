c6997a6bd7120c53b8f60dffbc2b190230973aec
Keerthanakulal22 <keerthana.23cs057@sode-edu.in>
2024-11-12 14:43:26 +0530

    Add files via upload

---
+++ M10.java
@@ -0,0 +1,17 @@
+
+package pkg1;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+public class M10 {
+     public static void main(String[] args)throws FileNotFoundException{
+        File f1=new File("names.txt");
+        Scanner sc1=new Scanner(f1);
+        String name="";
+        tools1 t1=new tools1();
+        while(sc1.hasNext()){
+            name=sc1.nextLine();
+            System.out.println(t1.reverse(name));
+        }
+     }
+}
